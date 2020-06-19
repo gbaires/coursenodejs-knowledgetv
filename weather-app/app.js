b7b6ece408276511edce6dbb97e960bd150c833b
Gabriel Aires <gabriel.aires.pt@gmail.com>
2020-06-19 15:22:01 -0300

    Callback Abstraction Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,12 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const url = 'http://api.weatherstack.com/current?access_key=d9f0e392a692320c7647bf70d6f97104&query=-22.911'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         const descrip = response.body.current.weather_descriptions[0]
-//         const temp = response.body.current.temperature
-//         const precip = response.body.current.precip
-//         console.log(descrip + '. It is currently ' + temp + ' degrees out. There is a ' + precip + '% chance of rain.')
-//     }
-// })
+const forecast = require('./utils/forecast')
 
 geocode('Rio de Janeiro', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+forecast(-22.911, -43.2094, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
